f43de7446bc12d2bfd06c541c4c650bfcf1a13b5
Iva <60818270+iva7777@users.noreply.github.com>
2024-04-30 11:02:02 +0300

    fix the calculateAverageRating logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,11 +11,6 @@ import { FormsModule } from '@angular/forms';
 })
 export class AppComponent {
   title = 'books-rating';
-  public index = 0;
-  public userRating = 0;
-  public totalRating = 0;
-  public count = 0;
-  public averageRating = 0;
   public isDisabled = true;
   public isDone = false;
 
@@ -23,93 +18,120 @@ export class AppComponent {
     {
       name: "Happy Place",
       description: "Happy Place is a 2023 novel written by American author Emily Henry ",
-      author: "Emily Henry"
+      author: "Emily Henry",
+      userRating: 0,
+      totalRating: 0,
+      ratingCount: 0,
+      averageRating: 0
     },
     {
       name: "Dune",
       description: "Dune is a 1965 epic science fiction novel by American author Frank Herbert",
-      author: "Frank Herbert"
+      author: "Frank Herbert",
+      userRating: 0,
+      totalRating: 0,
+      ratingCount: 0,
+      averageRating: 0
     },
     {
       name: "Bride",
       description: "#1 Indie Next Pick!A Hall of Fame LibraryReads pick!",
-      author: "Ali Hazelwood"
+      author: "Ali Hazelwood",
+      userRating: 0,
+      totalRating: 0,
+      ratingCount: 0,
+      averageRating: 0
     },
     {
       name: "The Woman: A Novel",
       description: "A #1 bestseller on The New York Times, USA Today, Washington Post, and Los Angeles Times!",
-      author: "Kristin Hannah"
+      author: "Kristin Hannah",
+      userRating: 0,
+      totalRating: 0,
+      ratingCount: 0,
+      averageRating: 0
     },
     {
       name: "Think Again: The Power of Knowing What You Don't Know",
       description: "This is such an important book that will teach you how to reassess your own belief system",
-      author: "Adam Grant"
+      author: "Adam Grant",
+      userRating: 0,
+      totalRating: 0,
+      ratingCount: 0,
+      averageRating: 0
     },
     {
       name: "Fourth Wing",
       description: "Fourth Wing is a 2023 new adult fantasy novel by American author Rebecca Yarros.",
-      author: "Rebecca Yarros"
+      author: "Rebecca Yarros",
+      userRating: 0,
+      totalRating: 0,
+      ratingCount: 0,
+      averageRating: 0
     },
     {
       name: "Table for Two: Fictions",
       description: "The always adored Amor Towles is back with all the essentials readers love",
-      author: "Amor Towles"
+      author: "Amor Towles",
+      userRating: 0,
+      totalRating: 0,
+      ratingCount: 0,
+      averageRating: 0
     },
     {
       name: "Crook Manifesto: A Novel",
       description: "The second book in Colson Whitehead's Harlem trilogy.",
-      author: "Colson Whitehead"
+      author: "Colson Whitehead",
+      userRating: 0,
+      totalRating: 0,
+      ratingCount: 0,
+      averageRating: 0
     },
     {
       name: "The Nickel Boys",
       description: "Colson Whitehead's follow-up novel to Underground Railroad won the Pullitzer Prize.",
-      author: "Colson Whitehead"
+      author: "Colson Whitehead",
+      userRating: 0,
+      totalRating: 0,
+      ratingCount: 0,
+      averageRating: 0
     },
     {
       name: "The Color Purple",
       description: "One of the cornerstones of modern American literature, The Color Purple is the often tragic.",
-      author: "Alice Walker"
+      author: "Alice Walker",
+      userRating: 0,
+      totalRating: 0,
+      ratingCount: 0,
+      averageRating: 0
     }
   ];
+  public currentIndex = 0;
 
-  public name = this.bookCollection[this.index].name;
-  public description = this.bookCollection[this.index].description;
-  public author = this.bookCollection[this.index].author;
+  public name = this.bookCollection[this.currentIndex].name;
+  public description = this.bookCollection[this.currentIndex].description;
+  public author = this.bookCollection[this.currentIndex].author;
+
 
   public getUserRating(rating: number){
-    this.userRating = rating;
+    this.bookCollection[this.currentIndex].userRating = rating;
     this.isDisabled = false;
   }
 
-  public calculateAverageRating(rating: number){
-    this.userRating = 0;
-    this.isDisabled = true;
+  public calculateAverageRating(){
 
-    //запази новите данни за книгата
-    this.bookCollection[this.index].name = this.name;
-    this.bookCollection[this.index].description = this.description;
-    this.bookCollection[this.index].author = this.author;
-    console.log(this.bookCollection[this.index]);
-    console.log(this.name);
-    this.index++;
-    if (this.index == this.bookCollection.length) {
-      this.averageRating = 0;
-      return;
-    }
-    this.totalRating += rating;
-    this.count++;
-    this.averageRating = this.totalRating / this.count;
-
-    this.name = this.bookCollection[this.index].name;
-    this.description = this.bookCollection[this.index].description;
-    this.author = this.bookCollection[this.index].author;
+    const currentBook = this.bookCollection[this.currentIndex];
+    currentBook.totalRating += currentBook.userRating;
+    currentBook.ratingCount++;
+    currentBook.averageRating = currentBook.totalRating /  currentBook.ratingCount;
+    this.currentIndex++;
   }
 
   public reset(){
-    this.index = 0;
-    this.name = this.bookCollection[this.index].name;
-    this.description = this.bookCollection[this.index].description;
-    this.author = this.bookCollection[this.index].author;
+    this.currentIndex = 0;
+    this.name = this.bookCollection[this.currentIndex].name;
+    this.description = this.bookCollection[this.currentIndex].description;
+    this.author = this.bookCollection[this.currentIndex].author;
   }
 
   public finishRating(){
